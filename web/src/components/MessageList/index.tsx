112948486f1255d637c7cb58294221f4b2ad84e2
Bryan Araujo <bryanmaraujo544@gmail.com>
2021-10-21 11:59:32 -0300

    finishing first version of app

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -2,6 +2,7 @@ import { api } from '../../services/api';
 import styles from './styles.module.scss';
 import logoImg from '../../assets/logo.svg';
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client'
 
 type Message = {
    id: string;
@@ -11,9 +12,31 @@ type Message = {
       avatar_url: string;
    }
 }
+const socket = io('http://localhost:4000');
+
+const messagesQueue: Message[] = [];
+
+socket.on('new_message', newMessage => {
+   messagesQueue.push(newMessage);
+})
 
 export function MessageList() {
    const [messages, setMessages] = useState<Message[]>([]);
+
+   useEffect(() => {
+      const timer = setInterval(() => {
+         if (messagesQueue.length > 0) {
+            setMessages(prevState => [
+               messagesQueue[0],
+               prevState[0],
+               prevState[1],
+            ].filter(Boolean));
+
+            messagesQueue.shift();
+         }
+      }, 3000)
+   }, [])
+
    useEffect(() => {
       (async () => {
          const res = await api.get<Message[]>('messages/last3');
@@ -34,7 +57,6 @@ export function MessageList() {
                      </div>
                      <span>{msg.user.name}</span>
                   </div>
-               
                </li>
             ))}
          </ul>
